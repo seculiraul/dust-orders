da7d8c4a4ea165f6bd21faec1e0a432bbd36502f
Raul <raulsmarius@yahoo.com>
2023-08-10 23:24:35 +0300

    minor change

--- src/controller/postOrder.js
+++ src/controller/postOrder.js
@@ -1,6 +1,7 @@
 const Order = require('../models/order')
 const catchAsync = require('../util/catchAsync')
 const producer = require('../messaging/Producer')
+const Products = require('../models/product')
 
 module.exports = catchAsync(async (req, res, next) => {
   const {
@@ -23,7 +24,7 @@ module.exports = catchAsync(async (req, res, next) => {
     deliveryOption,
   })
 
-  producer.publishMessage('Order_Created', order)
+  producer.publishMessage('Order_Created', products)
 
   res.status(201).json({
     message: 'success',
