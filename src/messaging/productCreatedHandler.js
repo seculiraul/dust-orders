9fe58b89d7f1c8f076008fc39f2866ac71fc4618
Raul <raulsmarius@yahoo.com>
2023-08-14 23:26:56 +0300

    Added new route and minor consumer changes

--- src/messaging/productCreatedHandler.js
+++ src/messaging/productCreatedHandler.js
@@ -2,11 +2,9 @@ const Product = require('../models/product')
 module.exports = async (productInfo) => {
   const product = await Product.findById(productInfo._id)
 
-  console.log(product)
   if (!product) {
     const newProduct = new Product(productInfo)
     await newProduct.save()
-    console.log(newProduct)
   } else {
     await Product.findByIdAndUpdate(productInfo._id, productInfo)
   }
