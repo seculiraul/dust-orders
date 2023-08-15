0c403bb0a1a2aedad325318fdc9d07e36c7cb31b
Raul <raulsmarius@yahoo.com>
2023-08-15 16:26:07 +0300

    More Features in the Orders API

--- src/controller/getOrderById.js
+++ src/controller/getOrderById.js
@@ -2,7 +2,7 @@ const catchAsync = require('../util/catchAsync')
 const Order = require('../models/order')
 
 module.exports = catchAsync(async (req, res, next) => {
-  const order = await Order.findById(req.params.id)
+  const order = await Order.findById(req.params.id).select('--v')
 
   res.json({
     message: 'success',
